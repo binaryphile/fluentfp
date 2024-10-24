8a59ce913056c8b3d209cc7872ff557cbfbaa5df
Ted Lilley <ted.lilley@digi.com>
2024-10-24 02:18:17 -0400

    advanced option

--- examples/must.go
+++ examples/must.go
@@ -14,13 +14,13 @@ func main() {
 	home := must.Getenv("HOME")
 	fmt.Println("got", home, "for $HOME")
 
-	// panic if os.File returns an error
-	file := must.Get(os.Open(home + "/.profile"))
+	// panic if os.Open returns an error
+	file := must.Get(os.Open(home + "/.profile")) // TODO: need universal example
 	fmt.Println("opened file")
 
 	// panic if there is an error on close
 	err := file.Close()
-	must.BeNil(err)
+	must.BeNil(err) // you could call this on file.Close directly, but assigning to err is more readable
 	fmt.Println("closed file")
 
 	// show a panic
